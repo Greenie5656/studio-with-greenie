e780ca8bad6522acb82a982f5c23a68b33962a10
root <andrewgreen5656@gmail.com>
2025-04-23 10:45:05 +0100

    header elements complete

--- Studio with Greenie/src/components/Header.jsx
+++ Studio with Greenie/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../components/Header.css'
 import logo from '../assets/logo.png';
+import remixedLogo from '../assets/remixed-logo.png'
 
 function Header() {
     return (
         <header className="header">
+            <div className='studio-text'>BOOK THE STUDIO WITH</div>
             <div className="logo-container">
                 <img src={logo} alt="Logo" className="logo pulse" />
+                <img src={remixedLogo} alt="remixed for mos, aatw, bcd and dinky records" className='remixed-logo' />
             </div>
         </header>
     );
